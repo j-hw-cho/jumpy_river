8335583e55c1677115fd1b29508e3d3245f4a749
hw2cho <hyeiwon.cho@gmail.com>
2018-03-15 20:01:31 +0900

    init commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	private Animator playerAnimator;
+
+	// Use this for initialization
+	void Start () {
+		playerAnimator = this.gameObject.GetComponent<Animator>();
+		playerAnimator.SetBool("touch", false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			playerAnimator.SetBool("touch", true);
+
+		}
+	}
+}
