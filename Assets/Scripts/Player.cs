2970e10a7e2a67d19b9cb1f4c65c54775452b0e6
hw2cho <hyeiwon.cho@gmail.com>
2018-03-17 14:00:17 +0900

    place objects to scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,17 +5,47 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 	private Animator playerAnimator;
 
+	private bool jumpReady;
+	private bool isJumping;
+
 	// Use this for initialization
-	void Start () {
+	void OnEnable () {
 		playerAnimator = this.gameObject.GetComponent<Animator>();
 		playerAnimator.SetBool("touch", false);
+		playerAnimator.SetBool("jump", false);
+
+		jumpReady = false;
+		isJumping = false;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
 			playerAnimator.SetBool("touch", true);
-
+			jumpReady = true;
+			Debug.Log("READY");
 		}
+
+		if (jumpReady) {
+			if (Input.GetMouseButtonUp(0)) {
+				playerAnimator.SetBool("jump", true);
+				isJumping = true;
+				Debug.Log("JUMP");
+			}
+		} 
+
+		if (isJumping) {
+			if (playerAnimator.GetCurrentAnimatorStateInfo(0).IsName("basic")) {
+				// Jump animation finished
+				playerAnimator.SetBool("jump", false);
+				playerAnimator.SetBool("touch", false);
+				isJumping = false;
+				jumpReady = false;
+				Debug.Log("Animation Finished");
+
+			}
+		}
+
 	}
 }
