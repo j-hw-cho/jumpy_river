5f1237d4f6a173f156dea194bd44016b62abacc6
hw2cho <hyeiwon.cho@gmail.com>
2018-03-18 20:04:09 +0900

    add power slider

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -11,6 +11,16 @@ public class Arrow : MonoBehaviour {
 	float unitAngle;
 	Vector3 curAngle;
 
+	/** power bar **/
+	public GameObject powerBar;
+	public GameObject powerSlider;
+	bool isPowerBarEnabled;
+	bool addPower;
+
+	float scaleUnit;
+
+
+
 	// Use this for initialization
 	void OnEnable () {
 		arrowMR = this.gameObject.GetComponent<MeshRenderer>();
@@ -22,6 +32,11 @@ public class Arrow : MonoBehaviour {
 		curAngle = this.gameObject.transform.eulerAngles;
 
 		unitAngle = 1;
+
+		powerBar.SetActive(false);
+		isPowerBarEnabled = false;
+		addPower = true;
+		scaleUnit = 2;
 	}
 
 	
@@ -43,10 +58,30 @@ public class Arrow : MonoBehaviour {
 			this.transform.eulerAngles = newAngle;
 			curAngle = newAngle;
 		}
+
+		if (isPowerBarEnabled) {
+			Vector3 sliderScale = powerSlider.transform.localScale;
+			Vector3 newScale = Vector3.zero;
+			if (addPower) {
+				newScale = new Vector3(sliderScale.x, sliderScale.y, sliderScale.z + 0.01f * scaleUnit);
+				if (newScale.z >= 0.99f) {
+					addPower = false;
+				}
+			} else {
+				newScale = new Vector3(sliderScale.x, sliderScale.y, sliderScale.z - 0.01f * scaleUnit);
+				if (newScale.z <= 0.01f) {
+					addPower = true;
+				}
+
+			}
+			powerSlider.transform.localScale = newScale;
+		}
 	}
 
 	void disable() {
 		arrowMR.enabled = false;
+		isPowerBarEnabled = false;
+		powerBar.SetActive(false);
 		isEnabled = false;
 
 	}
@@ -61,6 +96,8 @@ public class Arrow : MonoBehaviour {
 
 	void pauseArrow() {
 		pause = true;
+		powerBar.SetActive(true);
+		isPowerBarEnabled = true;
 	}
 
 
